fa3f604ed7b1e15fa5689565f2254cf773d5b16c
GuillaumeFitzrichard <alfredhwolfe@gmail.com>
2020-06-18 22:14:58 -0600

    database created and seeded with random data on startup

--- server/db/index.js
+++ server/db/index.js
@@ -1,9 +1,70 @@
 const mysql = require('mysql');
-const mysqlConfig = require('./config.js');
+const tableConfig = require('./config.js');
+const Promise = require('bluebird');
+const database = 'hipsyimage';
 
-const connection = mysql.createConnection(mysqlConfig);
-// first must connect!
-connection.connect();
+const connection = mysql.createConnection({
+  user: 'root',
+  password: 'p4j4m4sII!',
+  database: 'hipsyimage',
+  multipleStatements: true,
+});
+
+const db = Promise.promisifyAll(connection, { multiArgs: true });
+const masterSeed = () => {
+  const seedDb1 = (value1) => {
+    const insertQuery1 = 'INSERT INTO item (department) VALUES (?)';
+    db.queryAsync(insertQuery1, [value1], (error, results) => {
+      if (error) {
+        console.log('error seeding', error);
+      } else {
+        console.log('suck-sess', results);
+      }
+    });
+};
+
+  const seedDb2 = (value2) => {
+    const insertQuery2 = 'INSERT INTO image (image_url, item_id, product_id) VALUES ("www.placeimg.com/700/700/any", ?, ?)';
+    db.queryAsync(insertQuery2, [value2, value2], (error, results) => {
+      if (error) {
+        console.log('error seeding', error);
+      } else {
+        console.log('suck-sess', results);
+      }
+    });
+};
+
+  const randomImageNumber = (id) => {
+    let imageCount = Math.random() * (10 - 2) + 2;
+    for (let j = 0; j <= imageCount; j += 1) {
+      seedDb2(id);
+    }
+};
+
+  for (let i = 1; i <= 100; i += 1) {
+    if (i < 26) {
+      seedDb1('clothing');
+      randomImageNumber(i);
+    } else if (i < 51) {
+      seedDb1('art');
+      randomImageNumber(i);
+    } else if (i < 76) {
+      seedDb1('jewelry');
+      randomImageNumber(i);
+    } else if (i <= 100) {
+      seedDb1('miscellaneous');
+      randomImageNumber(i);
+    }
+  }
+};
+
+db.connectAsync()
+  .then(() => console.log(`Connected to ${database} as ID ${db.threadId}`))
+  .then(() => db.queryAsync(`DROP DATABASE IF EXISTS ${database}`))
+  .then(() => db.queryAsync(`CREATE DATABASE ${database}`))
+  .then(() => db.queryAsync(`USE ${database}`))
+  .then(() => tableConfig(db))
+  .then(() => masterSeed());
 
 // const getAllImages = function(callback) {
 //   const queryString = 'SELECT * FROM image';
@@ -16,4 +77,4 @@ connection.connect();
 //   });
 // };
 
-module.exports = connection;
+module.exports = db;
--- server/db/schema.sql
+++ server/db/schema.sql
@@ -1,60 +1,55 @@
-DROP DATABASE IF EXISTS hipsyimage;
+-- DROP DATABASE IF EXISTS hipsyimage;
 
-CREATE DATABASE hipsyimage;
+-- CREATE DATABASE hipsyimage;
 
-USE hipsyimage;
+-- USE hipsyimage;
 
-DROP TABLE IF EXISTS item;
+-- DROP TABLE IF EXISTS item;
 		
-CREATE TABLE item (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  department VARCHAR(50)
-);
+-- CREATE TABLE IF NOT EXISTS item (
+--   id INT AUTO_INCREMENT PRIMARY KEY,
+--   department VARCHAR(50)
+-- );
 
--- ---
--- Table 'image'
--- 
--- ---
+-- -- ---
+-- -- Table 'image'
+-- -- 
+-- -- ---
 
-DROP TABLE IF EXISTS image;
+-- DROP TABLE IF EXISTS image;
 		
-CREATE TABLE image (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  image_url VARCHAR(100),
-  item_id INTEGER,
-  product_id INTEGER,
-  CONSTRAINT fk_categorya
-  FOREIGN KEY (item_id)
-  REFERENCES item(id)
-    ON UPDATE CASCADE
-    ON DELETE CASCADE
-  -- CONSTRAINT fk_categoryb
-  -- FOREIGN KEY (product_id)
-  -- REFERENCES item(id)
-  --   ON UPDATE CASCADE
-  --   ON DELETE CASCADE
-);
+-- CREATE TABLE IF NOT EXISTS image (
+--   id INT AUTO_INCREMENT PRIMARY KEY,
+--   image_url VARCHAR(100),
+--   item_id INTEGER,
+--   product_id INTEGER,
+--   CONSTRAINT fk_categorya
+--   FOREIGN KEY (item_id)
+--   REFERENCES item(id)
+--     ON UPDATE CASCADE
+--     ON DELETE CASCADE
+-- );
 
--- ---
--- Foreign Keys 
--- ---
+-- -- ---
+-- -- Foreign Keys 
+-- -- ---
 
 
 
 
 
--- ---
--- Table Properties
--- ---
+-- -- ---
+-- -- Table Properties
+-- -- ---
 
--- ALTER TABLE `item` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE `image` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- -- ALTER TABLE `item` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- -- ALTER TABLE `image` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
--- ---
--- Test Data
--- ---
+-- -- ---
+-- -- Test Data
+-- -- ---
 
--- INSERT INTO `item` (`id`,`product_id`,`department`) VALUES
--- ('','','');
--- INSERT INTO `image` (`id`,`image_url`,`item_id`) VALUES
--- ('','','');
+-- -- INSERT INTO `item` (`id`,`product_id`,`department`) VALUES
+-- -- ('','','');
+-- -- INSERT INTO `image` (`id`,`image_url`,`item_id`) VALUES
+-- -- ('','','');
