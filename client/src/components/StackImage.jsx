5f1b0b157ab75f34c83ec2c48a2f229bce7c3123
GuillaumeFitzrichard <alfredhwolfe@gmail.com>
2020-06-25 22:12:15 -0600

    slowly going

--- client/src/components/StackImage.jsx
+++ client/src/components/StackImage.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const StackImage = (props) => {
   console.log('props in stackimg', props);
+
+  console.log('feature', feature);
+  console.log('whats this', typeof `${props.activeIndex}`);
   return (
     <div className="StackImage">
-      <img src={props.source} alt={props.product_id} />
+      <img id={props.imageIndex} src={props.source} alt={props.product_id} />
     </div>
   );
 };
